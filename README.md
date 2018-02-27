4d84caa74ffd0dc02acf688b6395bd47f8c85b96
Dustin Dowell <dustindowell22@gmail.com>
2018-02-27 03:17:31 -0600

    commit project

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# hyperapp-object-view-demo
+
+# <img src=favicon.png height=24> @whaaaley/hyperapp-object-view-demo
+
+> A Hyperapp component that renders JavaScript objects with syntax highlighting.
+
+![screenshot](screenshot.png)
+
+## Install and Setup
+
+```
+make setup
+```
+
+## Scripts
+
+Ensure you have GNU make version 3.8.2 or above.
+
+[See `Makefile` for details.](Makefile)
+
+### Start development
+
+Start a development server at [`http://localhost:3000/`](http://localhost:3000/) and watch for changes.
+
+```
+make start
+```
+
+### Build for production
+
+Set `NODE_ENV='production'` in your `.env` file.
+
+```
+make
+```
+
+## Deployment
+
+This project uses Netlify to deploy when commits are pushed to master.
+
+[View the Netlify dashboard for this project.](https://app.netlify.com/sites/hyperapp-object-view-demo)
---
+++ src/app.js
@@ -0,0 +1,51 @@
+
+'use strict'
+
+import { h, app } from 'hyperapp'
+import { ObjectView } from '@whaaaley/hyperapp-object-view'
+
+const state = {
+  array: [
+    'value0',
+    'value1',
+    [
+      'value0',
+      'value1'
+    ],
+    {
+      key0: 'value0',
+      key1: 'value1'
+    }
+  ],
+  boolean: false,
+  function: x => x,
+  null: null,
+  number: 1234,
+  object: {
+    key0: 'value0',
+    key1: 'value1',
+    key2: {
+      key0: 'value0',
+      key1: 'value1'
+    },
+    key3: [
+      'value0',
+      'value1'
+    ]
+  },
+  string: 'foobar',
+  undefined: void 0
+}
+
+const actions = {}
+
+const view = (...args) =>
+  h('div', { class: 'app' }, [
+    h('img', { src: 'images/hyperapp.png' }),
+    ObjectView({
+      key: 'state',
+      value: args[0]
+    })
+  ])
+
+app(state, actions, view, document.body)
